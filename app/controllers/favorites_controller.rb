1b82c9a35d812a0ef6d1d6a040fd5d94823d4893
EC2 Default User <ec2-user@ip-172-31-8-216.ap-northeast-1.compute.internal>
2023-06-09 07:55:35 +0000

    Initial Commit

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,25 @@
+class FavoritesController < ApplicationController
+  def create
+    # if !Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+      @book = Book.find(params[:book_id])
+      favorite = current_user.favorites.new(book_id: @book.id)
+      favorite.save
+      render 'replace_btn'
+    # end
+  end
+
+  def destroy
+    # if Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+      @book = Book.find(params[:book_id])
+      favorite = current_user.favorites.find_by(book_id: @book.id)
+      favorite.destroy
+      render 'replace_btn'
+    # end
+  end
+  
+  private
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :book_id)
+  end
+end
