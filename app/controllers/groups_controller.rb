67b050be3bec1e13daed944a989f65064b2b6c50
EC2 Default User <ec2-user@ip-172-31-8-216.ap-northeast-1.compute.internal>
2023-08-01 08:49:06 +0000

    9c完成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -51,6 +51,19 @@ class GroupsController < ApplicationController
     end
   end
   
+  # メール機能
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+  
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@mail_title,@mail_content,group_users).deliver
+  end
+  
   
   def group_params
     params.require(:group).permit(:name, :introduction, :image)
