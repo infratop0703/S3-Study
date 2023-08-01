cad602321ddabb48ef4b7b356b559a340d8bbb05
EC2 Default User <ec2-user@ip-172-31-8-216.ap-northeast-1.compute.internal>
2023-08-01 07:35:41 +0000

    8c完成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,18 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
   
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user #グループに現在のユーザーを追加
+    redirect_to  groups_path
+  end
+  
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user)
+    redirect_to groups_path
+  end
+  
   def new
     @group = Group.new
   end
@@ -19,6 +31,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.users << current_user
     if @group.save
       redirect_to groups_path
     else
